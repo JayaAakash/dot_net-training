209c36f87fc797a94a48618505594872e0523ec6
JayaAakash <160754826+JayaAakash@users.noreply.github.com>
2024-03-31 09:56:29 -0700

    Add files via upload

---
+++ dotnet-training/ADO dot net/linq_to_sql/linq_to_sql/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace linq_to_sql
+{
+    class Program
+    {
+        static DataClasses1DataContext hdc = new DataClasses1DataContext();
+        static void Main(string[] args)
+        {
+            var client = hdc.clients.ToList();
+            foreach (var item in client)
+            {
+                Console.WriteLine($"client name :{item.cname}, client ID: {item.cname}, client address: {item.address}");
+            }
+            Console.ReadLine();
+        }
+    }
+}
